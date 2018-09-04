4fca5057346bb351379e5c0d0d163034cf4a6b6a
Scott Weinstein <sweinstein27@gmail.com>
2018-09-04 14:57:05 +0000

    automatically backed up by learn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,4 @@
-class StudnetController < ApplicationController
+class StudentController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
